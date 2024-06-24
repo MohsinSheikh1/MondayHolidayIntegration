f69b0b4c96fde6adc6ac83a7d1c2d177b3342730
MohsinSheikh1 <sheikhmohsin181@gmail.com>
2024-06-25 04:30:27 +0500

    "Updated health.js to import and initialize Logger from @mondaycom/apps-sdk, and added code.tar.gz file"

--- routes/health.js
+++ routes/health.js
@@ -1,6 +1,10 @@
 const router = require("express").Router();
 const mondayRoutes = require("./monday");
 
+const { Logger } = require("@mondaycom/apps-sdk");
+const logTag = "Health";
+const logger = new Logger(logTag);
+
 router.use("/monday", mondayRoutes);
 
 router.get("/", (req, res) => {
