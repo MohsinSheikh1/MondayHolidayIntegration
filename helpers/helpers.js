a984ef4976eb3b949623629c25cf28238eb7bec4
MohsinSheikh1 <sheikhmohsin181@gmail.com>
2024-06-25 01:58:50 +0500

    env variables

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -1,19 +1,24 @@
-import { EnvironmentVariablesManager } from "@mondaycom/apps-sdk";
+const { EnvironmentVariablesManager } = require("@mondaycom/apps-sdk");
 
 const secretManager = new EnvironmentVariablesManager();
 
 const DEVELOPMENT_ENV = "development";
 const NODE_ENV = "NODE_ENV";
 
-export const isDevelopmentEnv = () => {
+const isDevelopmentEnv = () => {
   const currentEnv = (getSecret(NODE_ENV) || DEVELOPMENT_ENV).toLowerCase();
   return currentEnv === "development";
 };
 
-export const getEnv = () =>
-  (getSecret(NODE_ENV) || DEVELOPMENT_ENV).toLowerCase;
+const getEnv = () => (getSecret(NODE_ENV) || DEVELOPMENT_ENV).toLowerCase;
 
-export const getSecret = (secretKey, options = {}) => {
+const getSecret = (secretKey, options = {}) => {
   const secret = secretManager.get(secretKey, options);
   return secret;
+};
+
+module.exports = {
+  getSecret,
+  isDevelopmentEnv,
+  getEnv,
 };
